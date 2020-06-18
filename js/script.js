e4d981569ca5d9f46b2b71d09a417f479db38da4
Artem <theuselessn@gmail.com>
2020-06-18 15:38:39 +0600

    first review fixes (almost final)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-// modal
+// modal show
 var subButton = document.querySelector(".sub-form-trigger");
 var subOverlay = document.querySelector(".supscription-overlay");
 var subForm = document.querySelector(".supscription-form");
 var subOverlayClose = document.querySelector(".modal-close");
 var feedLogin = document.querySelector(".feedback-login");
+var feedEmail = document.querySelector(".feedback-email");
 
 subButton.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -24,29 +25,44 @@ window.addEventListener("keydown", function (evt) {
     if (subOverlay.classList.contains("overlay-show")) {
       subOverlay.classList.remove("overlay-show");
       subForm.classList.remove("modal-show");
+      subForm.classList.remove("modal-error");
     }
   }
 });
 
-//slider
+// modal error
+subForm.addEventListener("submit", function (evt) {
+  if (!feedLogin.value || !feedEmail.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести логин и пароль");
+    subForm.classList.remove("modal-error");
+    subForm.offsetWidth = subForm.offsetWidth;
+    subForm.classList.add("modal-error");
+  }
+});
+// slider
 var wrapper = document.querySelector(".wrapper-main");
-var slideHiddenRadio = document.querySelectorAll(".slider-radio");
+var slideRadios = document.querySelectorAll(".slider-radio");
 var sliderContent = document.querySelectorAll(".slider-item");
 
-function sliderChanger() {
-  if (slideHiddenRadio[0].checked == true) {
+slideRadios.forEach((slideRadios) =>
+  slideRadios.addEventListener("change", () => sliderFunction())
+);
+
+function sliderFunction() {
+  if (slideRadios[0].checked == true) {
     wrapper.classList.remove("wrapper-two", "wrapper-three");
     sliderContent[0].classList.add("slide-current");
     sliderContent[1].classList.remove("slide-current");
     sliderContent[2].classList.remove("slide-current");
     wrapper.classList.add("wrapper-one");
-  } else if (slideHiddenRadio[1].checked == true) {
+  } else if (slideRadios[1].checked == true) {
     wrapper.classList.remove("wrapper-one", "wrapper-three");
     wrapper.classList.add("wrapper-two");
     sliderContent[1].classList.add("slide-current");
     sliderContent[0].classList.remove("slide-current");
     sliderContent[2].classList.remove("slide-current");
-  } else if (slideHiddenRadio[2].checked == true) {
+  } else if (slideRadios[2].checked == true) {
     wrapper.classList.remove("wrapper-one", "wrapper-two");
     wrapper.classList.add("wrapper-three");
     sliderContent[2].classList.add("slide-current");
